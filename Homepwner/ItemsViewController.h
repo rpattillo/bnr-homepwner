5fad85d263651fc32cfa2664152ef721f71e1f84
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-20 21:07:10 -0500

    Add ItemsViewController

---
+++ Homepwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Ricky Pattillo on 12/20/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/ItemsViewController.m
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Ricky Pattillo on 12/20/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+
+@implementation ItemsViewController
+
+@end
