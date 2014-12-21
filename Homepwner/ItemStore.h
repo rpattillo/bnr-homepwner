3e6db40953e7505e857584b5f41232eb9b354377
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-21 11:44:36 -0500

    Implement ItemStore as Singleton
    Not a fan of this pattern, at least not without a way to allow
    for dependency injection at the very least.  Following the example.

---
+++ Homepwner/ItemStore.h
@@ -0,0 +1,20 @@
+//
+//  ItemStore.h
+//  Homepwner
+//
+//  Created by Ricky Pattillo on 12/20/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Item;
+
+@interface ItemStore : NSObject
+
+@property (nonatomic, readonly) NSArray *allItems;
+
++ (instancetype)sharedStore;
+- (Item *)createItem;
+
+@end
---
+++ Homepwner/ItemStore.m
@@ -0,0 +1,74 @@
+//
+//  ItemStore.m
+//  Homepwner
+//
+//  Created by Ricky Pattillo on 12/20/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import "ItemStore.h"
+#import "Item.h"
+
+@interface ItemStore ()
+
+@property (nonatomic, strong) NSMutableArray *privateItems;
+
+@end
+
+
+@implementation ItemStore
+
+#pragma mark - Initializers
+
++ (instancetype)sharedStore
+{
+   static ItemStore *sharedStore = nil;
+   
+   if (!sharedStore) {
+      sharedStore = [[self alloc] initPrivate];
+   }
+   
+   return sharedStore;
+}
+
+
+- (instancetype)init
+{
+   @throw [NSException exceptionWithName:@"Singleton"
+                                  reason:@"use +[ItemStore sharedStore]"
+                                userInfo:nil];
+   return nil;
+}
+
+
+- (instancetype)initPrivate
+{
+   self = [super init];
+   if ( self ) {
+      _privateItems = [[NSMutableArray alloc] init];
+   }
+   
+   return self;
+}
+
+
+#pragma mark - Accessors
+
+- (NSArray *)allItems
+{
+   return self.privateItems;
+}
+
+
+#pragma mark -
+
+- (Item *)createItem
+{
+   Item *item = [Item randomItem];
+   [self.privateItems addObject:item];
+   
+   return item;
+}
+
+
+@end
