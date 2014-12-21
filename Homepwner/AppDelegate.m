d0526deb7f5c63e3d3abc739bbbd8338812d1efe
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-20 21:36:21 -0500

    Replace main storyboard with items vew controller

--- Homepwner/AppDelegate.m
+++ Homepwner/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ItemsViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,8 +16,15 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-   // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+   
+   ItemsViewController *itemsVC = [[ItemsViewController alloc] init];
+   self.window.rootViewController = itemsVC;
+   
+   [self.window makeKeyAndVisible];
+   
    return YES;
 }
 
